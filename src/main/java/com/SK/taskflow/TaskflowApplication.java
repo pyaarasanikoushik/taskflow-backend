c563aaf3f7238760ddd5562fb30ff75755a94f82
koushik pyarasani <koushikpyarasani@gmail.com>
2026-06-06 23:59:34 +0530

    Setup PostgreSQL integration and user entity module

--- src/main/java/com/SK/taskflow/TaskflowApplication.java
+++ src/main/java/com/SK/taskflow/TaskflowApplication.java
@@ -2,11 +2,8 @@ package com.SK.taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {DataSourceAutoConfiguration.class}
-)
+@SpringBootApplication
 public class TaskflowApplication {
 
 	public static void main(String[] args) {
