b0905569c08e5fbc9770be3eed495a270ef23430
koushik pyarasani <koushikpyarasani@gmail.com>
2026-08-07 23:28:53 +0530

    Introduce service layer and constructor dependency injection

--- src/main/java/com/SK/taskflow/controller/UserController.java
+++ src/main/java/com/SK/taskflow/controller/UserController.java
@@ -1,15 +1,23 @@
 package com.SK.taskflow.controller;
 
 import com.SK.taskflow.dto.UserRegistrationRequest;
+import com.SK.taskflow.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/register")
     public String register(@RequestBody UserRegistrationRequest request) {
 
-        return "Welcome " + request.getName();
+        return userService.registerUser(request);
+
     }
 }
